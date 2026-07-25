f20ca4f3d2f71a2dbb04e046738d792397d963a6
MarkKovacs08 <markkovacs20021113@gmail.com>
2026-07-25 14:36:05 -0400

    Update Fridge.cs

--- Assets/Scripts/Fridge.cs
+++ Assets/Scripts/Fridge.cs
@@ -15,4 +15,19 @@ public class Fridge : MonoBehaviour
     {
         
     }
+
+    public void getfood(){
+
+        if (PlayerStats.Instance.Condition < 100)
+        {
+            for(int i =0; i <3; i++){
+            WorldTimer.Instance.TickForward.Invoke();
+            }
+            PlayerStats.Instance.Condition += 1;
+            if (PlayerStats.Instance.Condition > 100)
+            {
+                PlayerStats.Instance.Condition = 100;
+            }
+        }
+    }
 }
