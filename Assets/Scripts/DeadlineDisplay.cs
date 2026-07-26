2f6c4541629b6a50ff2b02e646c680484288cb80
s-france <sfrance1218@gmail.com>
2026-07-26 11:19:58 -0400

    deadline

---
+++ Assets/Scripts/DeadlineDisplay.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DeadlineDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = (WorldTimer.Instance.EndTime - WorldTimer.Instance.CurrentTimeslot) + " Hours Remain";
+        
+    }
+}
